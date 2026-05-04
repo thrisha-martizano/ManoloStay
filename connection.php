42de8bd635176682a3869a826d4a2b4dc1254091
thrisha martizano <martizanothrisha@gmail.com>
2026-05-04 19:15:55 +0800

    connection

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+// Step 1: Set your database connection details
+$servername = "localhost"; 
+$username = "root";        
+$password = "";            
+$database = "manolostay_db";
+
+// Step 2: Create the connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Step 3: Check if the connection was successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully!";
+?>
